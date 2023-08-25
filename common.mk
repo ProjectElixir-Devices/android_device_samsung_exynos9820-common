d23579f75d37309b40ec3a5888a310e7d1412ff7
LeDrew2017 <LeDrew101806@gmail.com>
2023-08-25 17:55:55 -0400

    exynos9820-common:common.mk: set PRODUCT_USE_SCUDO := true
    Set "PRODUCT_USE_SCUDO := true" to common.mk to use scudo for some reason jemalloc causes camera freezing in exynos9820.

--- common.mk
+++ common.mk
@@ -338,3 +338,5 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+    
+PRODUCT_USE_SCUDO := true    
